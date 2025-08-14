66e5f4154177d90fc54eaad8ad2adf7ebddd8daa
Stefan Krtolica <stefanstefankrtolica@gmail.com>
2025-08-14 07:41:33 +0200

    Add geocoding service

--- config/owm_config.py
+++ config/owm_config.py
@@ -1,14 +1,16 @@
 
 
 class OWMConfig:
+    """Class for storing OWM related configuration."""
+    
     CURRENT_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
     GEOCODING_URL = "http://api.openweathermap.org/geo/1.0/direct"
     
-    MAX_RETRIES = 5 # Number of retries fir OWM requests 
+    MAX_RETRIES = 5 # Number of retries for OWM requests 
     TIMEOUT = 5.0 # Timeout seconds for OWM requests
     
     class GeocodingConfig:
-        LIMIT = 1 # Return only coordinates for the first result from Geocoding API
+        LIMIT = 1 # Valid range 1-5
 
     
     class CurrentWeatherConfig:
---
+++ services/owm_geocoding_service.py
@@ -0,0 +1,67 @@
+from pydantic import ValidationError
+
+from exceptions import OWMDataValidationException, OWMDataException
+
+from config.owm_config import OWMConfig
+from models.owm_models import OWMGeocodingResponseModel
+from utils.logger import get_logger
+
+from .owm_base_service import OWMBaseService
+
+
+logger = get_logger(__name__)
+
+
+class OWMGeocodingService(OWMBaseService):
+    
+    async def get_coordinates(self, city: str, multiple_results: bool = False):
+        """Get coordinates for a city (returns most relevant match)."""
+        if not city:
+            raise ValueError("City name must be provided.")
+        normalized_city = city.strip().lower()
+
+        data = await self._make_request(
+            OWMConfig.GEOCODING_URL,
+            params={"q": normalized_city, "limit": OWMConfig.GeocodingConfig.LIMIT}
+        )
+
+        if not data:
+            error_msg = f"No geocoding data found for city: {city}"
+            logger.warning(error_msg)
+            raise OWMDataException(error_msg)
+        
+        result = []
+        for item in data:
+            try:
+                coords = OWMGeocodingResponseModel(**item)
+                result.append(coords)
+                logger.info(f"Added coordinates ({coords.lat} - {coords.lon}) for city: {city}.")
+            except ValidationError as e:
+                logger.warning(f"Invalid geocoding item {item}. Validation failed: {e}.")
+                continue
+
+        if not result:
+            error_msg = f"No valid geocoding results returned for {city}."
+            logger.error(error_msg)
+            raise OWMDataException(error_msg)
+
+        logger.info(f"{len(result)} geocoding items passed validation for {city}.")
+
+        # Return only the first/most relevant result if multiple_results flag is False, all results otherwise.
+        return result[0] if not multiple_results else result 
+    
+
+            
+
+
+
+        
+        
+        
+        
+
+
+        
+
+        
+
